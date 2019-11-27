1ac1e7d1dd498c87db3afb770c260a41104b39e8
cristhianschug <schugcris@gmail.com>
2019-11-27 17:21:17 -0300

    firt-commit

---
+++ AppReserva/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppReserva.MvcApplication" Language="C#" %>
---
+++ AppReserva/Models/Reserva.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppReserva.Models
+{
+    public class Reserva
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+    }
+}
