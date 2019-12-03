9a4aafebccae00f82012e807128bcbab2ef51b04
cristhianschug <schugcris@gmail.com>
2019-12-03 16:31:13 -0300

    Criação de models ItemReserva e Reserva

--- AppReserva/Models/Reserva.cs
+++ AppReserva/Models/Reserva.cs
@@ -8,6 +8,10 @@ namespace AppReserva.Models
     public class Reserva
     {
         public int ID { get; set; }
+        public int IDItemReserva { get; set; }
         public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataHoraEntrada { get; set; }
+        public DateTime DataHoraSaida { get; set; } 
     }
 }
